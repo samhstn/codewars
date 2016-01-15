cd52bdd6425852071927a2be70d12fa8ce13dde9
shouston3 <shouston3@me.com>
2016-01-15 22:35:02 +0000

    remove

--- peopleOnABus.js
+++
@@ -1,9 +0,0 @@
-number = function(busStops){
-  var count=0;
-  for(i=0;i<busStops.length;i++){
-    count+=busStops[i][0]-busStops[i][1];
-  }
-  return count;
-};
-
-console.log(number([[3,0],[9,1],[4,8],[12,2],[6,1],[7,8]]));
--- program.js
+++
@@ -1,3 +0,0 @@
-x = [1, 2, 3, 4].reduce((prev, curr) => prev * curr, 1);
-
-console.log(x);
