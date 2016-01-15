7b6f74a7f05a343817474c70908d73f4c8540165
shouston3 <shouston3@me.com>
2016-01-15 22:47:40 +0000

    added people on a bus kata

---
+++ peopleOnABus.js
@@ -0,0 +1,9 @@
+number = function(busStops){
+  var count=0;
+  for(i=0;i<busStops.length;i++){
+    count+=busStops[i][0]-busStops[i][1];
+  }
+  return count;
+};
+
+console.log(number([[3,0],[9,1],[4,8],[12,2],[6,1],[7,8]]));
